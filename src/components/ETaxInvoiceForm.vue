e2e6a35a7f5cdb14bf240861d020dde5b790177a
nutchy <58070021@kmitl.ac.th>
2018-04-15 15:54:59 +0700

    Import PayeeForm into ETaxInvoiceForm.

--- src/components/ETaxInvoiceForm.vue
+++ src/components/ETaxInvoiceForm.vue
@@ -3,21 +3,26 @@
     <br>
       <h1>ใบกำกับภาษีอิเล็กทรอนิกส์</h1>
       <seller-form :seller="seller"></seller-form>
+      <payee-form :payee="payee"></payee-form>
   </div>
 </template>
 
 <script>
 import SellerForm from '@/components/forms/SellerForm';
 import SellerData from '@/data/Seller.data';
+import PayeeForm from '@/components/forms/PayeeForm';
+import PayeeData from '@/data/Payee.data';
 
 export default {
   data() {
     return {
       seller: SellerData.data,
+      payee: PayeeData.data,
     };
   },
   components: {
     SellerForm,
+    PayeeForm,
   },
 };
 </script>
