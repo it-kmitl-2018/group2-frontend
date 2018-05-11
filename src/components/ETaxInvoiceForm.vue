a77177524f0f67e95d34d79e9d5eb104d2ee915e
nutchy <58070021@kmitl.ac.th>
2018-05-11 23:35:54 +0700

    Import PayerForm Component into ETaxInvoiceForm.

--- src/components/ETaxInvoiceForm.vue
+++ src/components/ETaxInvoiceForm.vue
@@ -6,6 +6,7 @@
         <b-row>
           <b-col><seller-form :seller="seller"></seller-form></b-col>
           <b-col><payee-form :payee="payee"></payee-form></b-col>
+          <b-col><payer-form :payer="payer"></payer-form></b-col>
         </b-row>
       </b-card>
     </b-container>
@@ -17,6 +18,8 @@ import SellerForm from '@/components/forms/SellerForm';
 import SellerData from '@/data/Seller.data';
 import PayeeForm from '@/components/forms/PayeeForm';
 import PayeeData from '@/data/Payee.data';
+import PayerForm from '@/components/forms/PayerForm';
+import PayerData from '@/data/Payer.data';
 import '../../node_modules/bootstrap/dist/css/bootstrap.css';
 import '../../node_modules/bootstrap-vue/dist/bootstrap-vue.css';
 
@@ -25,11 +28,13 @@ export default {
     return {
       seller: SellerData.data,
       payee: PayeeData.data,
+      payer: PayerData.data,
     };
   },
   components: {
     SellerForm,
     PayeeForm,
+    PayerForm,
   },
 };
 </script>
