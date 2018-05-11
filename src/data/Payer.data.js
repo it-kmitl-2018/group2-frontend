aaeeb3dd19d5fccad6a4486ce48ede39ad1917e7
nutchy <58070021@kmitl.ac.th>
2018-05-11 23:34:58 +0700

    Add the Payer data file.

---
+++ src/data/Payer.data.js
@@ -0,0 +1,33 @@
+export default {
+  data: {
+    party: {
+      id: '',
+      globalID: '',
+      name: '',
+    },
+    tax: {
+      id: '',
+    },
+    contact: {
+      personName: '',
+      departmentName: '',
+      email: '',
+      phoneNumber: '',
+    },
+    address: {
+      postalCode: '',
+      building: '',
+      addressLineOne: '',
+      addressLineTwo: '',
+      addressLineThree: '',
+      addressLineFour: '',
+      addressLineFive: '',
+      street: '',
+      district: '',
+      subDistrict: '',
+      countryID: '',
+      provinceID: '',
+      buildingNumber: '',
+    },
+  },
+};
