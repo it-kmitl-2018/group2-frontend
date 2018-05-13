cb5db6d9b6da72018952b2f3b736597ebf2ccb2e
J <saii16320@gmail.com>
2018-05-13 16:32:05 +0700

    Add BuyerForm component.

---
+++ src/components/forms/BuyerForm.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h2>ผู้ซื้อ</h2>
+    <b-form>
+      <party-form :party="buyer.party"></party-form>
+      <tax-form :tax="buyer.tax"></tax-form>
+      <contact-form :contact="buyer.contact"></contact-form>
+      <address-form :address="buyer.address"></address-form>
+    </b-form>
+  </div>
+</template>
+
+<script>
+import PartyForm from './party/Party';
+import TaxForm from './party/Tax';
+import ContactForm from './party/Contact';
+import AddressForm from './party/Address';
+
+export default {
+  props: ['buyer'],
+  components: {
+    PartyForm,
+    TaxForm,
+    ContactForm,
+    AddressForm,
+  },
+};
+</script>
