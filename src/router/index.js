30ac77315eea33e5bd76b82a737d040983f393a8
nutchy <58070021@kmitl.ac.th>
2018-05-14 21:39:04 +0700

    Add the routing path for ETaxInvoiceConfirm.

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import ETaxInvoiceForm from '@/components/ETaxInvoiceForm';
+import ETaxInvoiceConfirm from '@/components/ETaxInvoiceConfirm';
 
 Vue.use(Router);
 
@@ -13,6 +14,11 @@ export default new Router({
       path: '/ETaxInvoiceForm',
       name: 'ETaxInvoiceForm',
       component: ETaxInvoiceForm,
+    },
+    {
+      path: '/ETaxInvoiceConfirm',
+      name: 'ETaxInvoiceConfirm',
+      component: ETaxInvoiceConfirm,
     },
   ],
 });
